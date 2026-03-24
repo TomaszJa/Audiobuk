ab51adf96b5358ca4ee88ce0c0eeccb7a3eeb477
Tomasz Jaszcz <t.jaszcz99@gmail.com>
2026-03-24 21:17:13 +0100

    search bar now displays hours:minutes:seconds

--- app/src/main/java/com/example/audiobuk/util/TimeUtils.kt
+++ app/src/main/java/com/example/audiobuk/util/TimeUtils.kt
@@ -1,14 +1,28 @@
 package com.example.audiobuk.util
 
+import java.util.Locale
+
 fun formatTime(ms: Long): String {
     val totalSeconds = ms / 1000
-    val minutes = totalSeconds / 60
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
     val seconds = totalSeconds % 60
-    return String.format("%02d:%02d", minutes, seconds)
+    
+    return if (hours > 0) {
+        String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds)
+    } else {
+        String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+    }
 }
 
 fun formatTimerRemaining(seconds: Long): String {
-    val mins = seconds / 60
-    val secs = seconds % 60
-    return String.format("%02d:%02d", mins, secs)
+    val h = seconds / 3600
+    val m = (seconds % 3600) / 60
+    val s = seconds % 60
+    
+    return if (h > 0) {
+        String.format(Locale.getDefault(), "%d:%02d:%02d", h, m, s)
+    } else {
+        String.format(Locale.getDefault(), "%02d:%02d", m, s)
+    }
 }
