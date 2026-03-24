a628d89980f17e677bcbf1249998b52efbd20479
Tomasz Jaszcz <t.jaszcz99@gmail.com>
2026-03-24 17:47:16 +0100

    Added new icon

--- app/src/main/java/com/example/audiobuk/ui/theme/Color.kt
+++ app/src/main/java/com/example/audiobuk/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.example.audiobuk.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Green80 = Color(0xFF81C784)
-val GreenGrey80 = Color(0xFFA5D6A7)
-val LightGreen80 = Color(0xFFC8E6C9)
+val Green80 = Color(0xFF2E7D32)
+val GreenGrey80 = Color(0xFF43A047)
+val LightGreen80 = Color(0xFF1B5E20)
 
 val Green40 = Color(0xFF2E7D32)
 val GreenGrey40 = Color(0xFF43A047)
