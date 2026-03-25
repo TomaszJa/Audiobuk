14bdeef2e6bc6b38efd6296c2eb8cc138a340068
Tomasz Jaszcz <t.jaszcz99@gmail.com>
2026-03-25 13:44:18 +0100

    Updated colors

--- app/src/main/java/com/example/audiobuk/ui/theme/Color.kt
+++ app/src/main/java/com/example/audiobuk/ui/theme/Color.kt
@@ -2,55 +2,48 @@ package com.example.audiobuk.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Tree Palette - Toned Down Greens (More Natural)
+// Tree Palette - Muted Forest Tones
 val DeepGreen = Color(0xFF1B5E20)
 val ForestGreen = Color(0xFF2E7D32)
-val EmeraldGreen = Color(0xFF2E7D32) // Toned down from 00C853
+val EmeraldGreen = Color(0xFF2E7D32)
 val MalachiteGreen = Color(0xFF388E3C)
-val LeafGreen = Color(0xFF4CAF50)
-val VibrantLeafGreen = Color(0xFF43A047)
+val LeafGreen = Color(0xFF43A047)
+val VibrantLeafGreen = Color(0xFF388E3C)
 val JungleGreen = Color(0xFF1B5E20)
-val NeonLeaf = Color(0xFF4CAF50) // Toned down from 39FF14, much more natural
-val MossGreen = Color(0xFF689F38)
+val NeonLeaf = Color(0xFF2E7D32)
+val MossGreen = Color(0xFF558B2F)
 
-// Tree Palette - Darker, Muted Browns
-val BarkBrown = Color(0xFF4E342E)
-val DeepBark = Color(0xFF2D1B18) // Darker than 3E2723
-val SaturatedSienna = Color(0xFF795548)
-val Mahogany = Color(0xFF5D4037)
-val GoldenBark = Color(0xFF6D4C41)
-val BurntUmber = Color(0xFF5D4037)
-val RichBark = Color(0xFF3E2723)
-val EarthBrown = Color(0xFF3E2723)
+// Dark Forest Greens (Replacing Browns)
+val DarkForestSecondary = Color(0xFF142918)
+val VeryDarkForest = Color(0xFF0D160E) 
+val DeepShadowGreen = Color(0xFF1B3022)
+val DarkMoss = Color(0xFF253D29)
+val MidnightGreen = Color(0xFF0A1F13)
 
-// Original/Base Palette
-val SageGreen = Color(0xFF81C784)
-val PaleGreen = Color(0xFFC8E6C9)
-
-// Dark Theme Palette - "Night Forest"
-val DarkPrimary = Color(0xFF4CAF50)
-val DarkOnPrimary = Color(0xFF0D120D)
+// Dark Theme Palette - "Midnight Forest"
+val DarkPrimary = Color(0xFF2E7D32)
+val DarkOnPrimary = Color(0xFFE8F5E9)
 val DarkPrimaryContainer = Color(0xFF1B5E20)
 val DarkOnPrimaryContainer = Color(0xFFC8E6C9)
-val DarkSecondary = Color(0xFF5D4037)
-val DarkOnSecondary = Color(0xFF2D1B18)
-val DarkSecondaryContainer = Color(0xFF3E2723)
-val DarkOnSecondaryContainer = Color(0xFFD7CCC8)
-val DarkBackground = Color(0xFF0D120D)
-val DarkSurface = Color(0xFF121812)
-val DarkSurfaceVariant = Color(0xFF1B241B)
-val DarkOnSurface = Color(0xFF81C784)
-val DarkOnBackground = Color(0xFF81C784)
+val DarkSecondary = Color(0xFF1B3022) // Dark Green instead of Brown
+val DarkOnSecondary = Color(0xFFE8F5E9)
+val DarkSecondaryContainer = Color(0xFF142918) // Darker Green instead of Brown
+val DarkOnSecondaryContainer = Color(0xFFA5D6A7)
+val DarkBackground = Color(0xFF080D08) // Almost black-green
+val DarkSurface = Color(0xFF0D120D)
+val DarkSurfaceVariant = Color(0xFF141A14)
+val DarkOnSurface = Color(0xFFA5D6A7)
+val DarkOnBackground = Color(0xFFA5D6A7)
 
 // Light Theme Palette - "Sunlit Tree"
 val LightPrimary = Color(0xFF2E7D32)
 val LightOnPrimary = Color.White
 val LightPrimaryContainer = Color(0xFFC8E6C9)
 val LightOnPrimaryContainer = Color(0xFF1B5E20)
-val LightSecondary = Color(0xFF5D4037)
+val LightSecondary = Color(0xFF388E3C)
 val LightOnSecondary = Color.White
-val LightSecondaryContainer = Color(0xFFEFE6DD)
-val LightOnSecondaryContainer = Color(0xFF4E342E)
+val LightSecondaryContainer = Color(0xFFE8F5E8)
+val LightOnSecondaryContainer = Color(0xFF1B5E20)
 val LightBackground = Color(0xFFF1F8F1)
 val LightSurface = Color(0xFFF9FBF9)
 val LightSurfaceVariant = Color(0xFFE8EDE8)
